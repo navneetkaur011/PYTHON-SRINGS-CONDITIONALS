6ae6f992080b3821a95692084d94650ec2368821
navneetkaur011 <navvkaurnanda@gmail.com>
2026-07-06 23:56:14 +0530

    Add string code

---
+++ 01_String.py
@@ -0,0 +1,9 @@
+# String is a data type that stores a sequence of characters.
+str1 = "This is a string.\n"
+str2 = 'Coding "Python"\n'
+str3 = """Roses are red,
+vials are blue,
+Python is awesome,
+so are you!"""
+print(str1,str2,str3) 
+ 
